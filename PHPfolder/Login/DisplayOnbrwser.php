f47a1be869955cc9bf751d8b861b7e36533cf441
Kelbessa Adugna <>
2025-03-08 21:31:10 +0300

    finish display table on browser

--- PHPfolder/Login/DisplayOnbrwser.php
+++ PHPfolder/Login/DisplayOnbrwser.php
@@ -6,18 +6,43 @@ $dbname = "Group5DB";
 
 $conn= new mysqli($servername, $username, $password,$dbname); 
 
-$sql = "select Id, Fname, Lname, Email From Student";
-$result =mysqli_query($conn, $sql);
-// outpu data for each row
+$sql = "select
+          Id, 
+          Fname, 
+          Lname, 
+          Email 
+          FROM Student";
 
-echo "<table border =1 ";
-echo "<tr> <td> ID </td> <td> Fname </td>  <td> Lname </td> <td> Email</td </tr> ";
+
+$result =mysqli_query($conn, $sql);
+
+// output data for each row
+echo "<p style='text-align: center; font-size: 30px;'>Display Table On Browser</p>";
+
+
+
+echo "<table border='1' style='margin: auto; border-collapse: collapse; width: 50%; text-align: center;font-size:20px;'>";
+
+echo "<tr> 
+
+        <td> ID </td> 
+        <td> Fname </td>  
+        <td> Lname </td> 
+        <td> Email</td 
+</tr> ";
+
 while($row=mysqli_fetch_assoc($result)){
-    echo "<tr>  <td>{$row["Id"]}</td>  <td>{$row["Fname"]}</td>  <td>{$row["Lname"]}</td>  <td>{$row["Email"]}</td>  </tr>";
+    echo "<tr> 
+            <td>{$row["Id"]}</td> 
+             <td>{$row["Fname"]}</td> 
+             <td>{$row["Lname"]}</td> 
+             <td>{$row["Email"]}</td>
+         </tr>";
 }
 
-
-
 echo "</table>";
+
+
+
 mysqli_close($conn);
 ?>
