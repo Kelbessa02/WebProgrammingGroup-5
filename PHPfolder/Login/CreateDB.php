ba37e425e360d9cb5ed904e806130422f71b776c
Kelbessa Adugna <>
2025-03-08 17:21:21 +0300

    insert table data

--- PHPfolder/Login/CreateDB.php
+++ PHPfolder/Login/CreateDB.php
@@ -1,8 +1,32 @@
 <?php
-require "DatabaseConnection.php";
-$sql = "create database Group5DB";
+$servername = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$dbname = "Group5DB";
+
+// Create a connection 
+$conn= new mysqli($servername, $username, $password,$dbname ); 
+// Check connection 
+if ($conn->connect_error) { 
+die("Connection failed: " . $conn->connect_error); 
+} 
+//$sql = "DROP database Group5DB";
+//$sql = "create database Group5DB";
+//$sql = "create table Student(
+    // Id int(6) unsigned auto_increment primary key,
+    // Fname Varchar(30)not null,   
+    // Lname varchar (30),
+    // Sex varchar(5) not null,
+    // Email varchar(50) not null,
+    // Password Varchar(50) not null
+    // )";
+    // $sql = "drop table student";
+   $sql ="insert into Student(Fname,Lname, Sex, password, Email) values('Kelbe','Adugna','Male', '1q2w3e', 'kelbessaadugna@gmail.com')";
+
+
+    
 if(mysqli_query($conn,$sql)){
-    echo "database is created succesfully";
+    echo "data is added succesfully";
 }else{
 echo "Error craeting database".mysqli_error($conn);
 }
---
+++ PHPfolder/Login/DataInsertion.php
@@ -0,0 +1,54 @@
+<?php
+
+
+// Prepare the SQL statement
+$sql = "Insert into Student(Fname, Lname, Sex,Email,  password) VALUES(?, ?, ?, ?, ?)";
+$stmt = $conn->prepare($sql);//those querry is used to take many input at a time
+
+// Bind parameters
+$stmt->bind_param("sssss", $Fname, $Lname, $Sex,$password,$Email);
+
+// Set parameters and execute
+$Fname = "Sena";
+$Lname = "Nifthalem";
+$Sex = "Female";
+$Email = "Sena@gmail.com";
+$password = "123qw";
+$stmt->execute();
+
+
+$Fname = "Addisu";
+$Lname = "Teshome";
+$Sex = "Male";
+$Email = "Addisu@gmail.com";
+$password = "12345";
+$stmt->execute();
+
+$Fname = "Robsan";
+$Lname = "H/Mikael";
+$Sex = "Male";
+$Email = "Robsan@gmail.com";
+$password = "54321";
+
+$stmt->execute();
+
+$Fname = "Seifu";
+$Lname = "Debebe";
+$Sex = "Male";
+$Email = "Seifu@gmail.com";
+$password = "12123";
+$stmt->execute();
+
+    
+
+// Check for errors
+if ($stmt->error) {
+    echo "Error inserting data: " . $stmt->error;
+} else {
+    echo "Data inserted successfully.";
+}
+
+// Close the statement and connection
+$stmt->close();
+mysqli_close($conn);
+?>
